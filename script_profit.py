4d4840a8ac255f3dbb0d1e720e0646e3fa65c74d
Agneska <aslusarc@student.42.fr>
2019-05-06 13:32:12 +0200

    try to improve fit function

--- script_profit.py
+++ script_profit.py
@@ -2,21 +2,45 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 
-def predict(X, theta):
+filename = "profit_data1.csv"
+indep = 'population'
+dep = "profit"
+'''
+filename = "price_data.csv"
+indep = "km"
+dep = "price"
+'''
+def hipothesis(X, theta):
     return X*theta[1] + theta[0]
 
 def fit(X, y, theta, alpha, num_iters):
     m = len(X)
+    #m = X.shape[0]
+    print("m = " , m)
+    print("alpha = " , alpha)
+    gradient = [0,0]
+    X0 = np.ones(len(X))
     for _ in range(num_iters):
-        error = predict(X, theta) - y
-        theta[0] = theta[0] - (alpha/m) * np.sum(error)
-        theta[1] = theta[1] - (alpha/m) * np.dot(error, X)
+        errors = hipothesis(X, theta) - y
+        print("errors : " , errors)
+        theta[0] = theta[0] - (alpha * np.sum(errors))/m
+        theta[1] = theta[1] - (alpha * np.dot(errors, X))/m
+        #theta[0] = theta[0] - (alpha * np.dot(errors, X0))/m
+        #theta[1] = theta[1] - (alpha * np.dot(errors, X))/m
+        print("theta :", theta)
+        #gradient[0] = alpha * np.dot(errors, X0)/ m
+        #gradient[1] = alpha * np.dot(errors, X)/ m
+        #print("gradient : " , gradient)
+        #theta[0] = theta[0] - gradient[0] 
+        #theta[1] = theta[1] - gradient[1]
+#       theta[1] = theta[1] - (alpha/m) * np.sum(error * X.T)
+        print("theta iter :" , theta)
     return theta
 
-def cost(X, y, theta):
-    m = len(X)
-    error = predict(X, theta) - y
-    cost = (1/(2*m))*(np.sum(error**2))
+
+
+def calcul_cost(errors, m):
+    cost = (1/(2*m))*(np.sum(errors**2))
     return cost
 
 def fit_with_cost(X, y, theta, alpha, num_iters):
@@ -24,27 +48,35 @@ def fit_with_cost(X, y, theta, alpha, num_iters):
     C_history = []
     T_history = []
     for _ in range(num_iters):
-        error = predict(X, theta) - y
-        print("error : " + str(error))
-        theta[0] = theta[0] - (alpha/m) * np.sum(error)
-        theta[1] = theta[1] - (alpha/m) * np.dot(error, X)
+        errors = hipothesis(X, theta) - y
+        print("errors : " + str(errors))
+        theta[0] = theta[0] - (alpha * np.sum(errors))/m
+        theta[1] = theta[1] - (alpha * np.dot(errors, X))/m
         t = theta.copy()
         T_history.append(t)
-        C_history.append(cost(X, y, theta))
+        total_cost = calcul_cost(errors, m)
+        C_history.append(total_cost)
     return theta, T_history, C_history
 
 def visualizeRegression(X, y, theta):
+    print("minX : " , min(X))
+    print("max X : " ,max(X))
+    print("min y : " , min(y))
+    print("max y : " , max(y))
+    print("theta : " , theta)
     plt.figure(1)
     ax = plt.axes()
-    ax.set_xlim([0,25])
-    ax.set_ylim([0,25])
+    ax.set_xlim([0 ,max(X)* 2])
+    ax.set_ylim([min(y)*0.9,max(y)*1.1])
     ax.scatter(X, y)
-    line_x = np.linspace(0,22.5, 20)
+    line_x = np.linspace(0,max(X)*1.1, 20)
+    print(line_x)
     line_y = theta[0] + line_x * theta[1]
+    print(line_y)
     ax.plot(line_x, line_y)
     plt.title('Function de regression', fontsize=18, fontweight='bold')
-    plt.xlabel('population', fontsize=14, fontweight='bold')
-    plt.ylabel('profit', fontsize=14, fontweight='bold')
+    plt.xlabel(indep, fontsize=14, fontweight='bold')
+    plt.ylabel(dep, fontsize=14, fontweight='bold')
     ax.grid(color='black', linestyle='-', linewidth=0.2)
     plt.show()
 
@@ -67,25 +99,31 @@ def visualizeTheta (T_history) :
     plt.show()
 
 def main(argv):  
-    data = pd.read_csv(argv)
-    data.plot.scatter('population', 'profit')
-    X = np.array(data['population'])
-    y = np.array(data['profit'])
+    data = pd.read_csv(argv, sep=",")
+#    print(type(data[indep][0]))
+    #data.plot.scatter(indep, dep)
     
+    
+    X = np.array(data[indep].astype(float))
+    y = np.array(data[dep].astype(float))
+    print("X = ", X)
+    print("y = ", y)
+    #X = (500000 - X)/1000
+    #print("X reversed = ", X)
     theta = np.zeros(2)
     print("main theta : " + str(theta))
-    Z = X*theta[1] + theta[0]
-    print("main Z : " + str(Z))
-    theta = np.zeros(2)
-    print("main theta : " + str(theta))
-    #theta = fit(X, y, theta, 0.02, 1000)
-    theta, T_history, J_history = fit_with_cost(X, y, theta, 0.02, 10)
-    print(theta)
+#    Z = X*theta[1] + theta[0]
+#    print("main first occurence de Z : " + str(Z))
+#    theta = [0, 0]
+#    print("main theta : " + str(theta))
+    theta = fit(X, y, theta, 0.002, 1000)
+    print("theta apres le fit" , theta)
+    #theta, T_history, J_history = fit_with_cost(X, y, theta, 0.02, 100)
     visualizeRegression(X, y, theta)
-    visualizeCost(J_history)
-    visualizeTheta (T_history) 
-    print("Prediction pour 5734: "+str(predict(5734, theta)))
+    #visualizeCost(J_history)
+    #visualizeTheta (T_history) 
+    print("Prediction pour 5734: "+str(hipothesis(5734, theta)))
 
 if __name__ == '__main__':
     #main(sys.argv[1])
-    main("ex1data1.csv")
+    main(filename)
