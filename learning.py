1ada14626d92be46d58f76931c8057346451c04d
Agnieszka SLUSARCZYK <aslusarc@e1r2p12.42.fr>
2019-05-22 16:24:45 +0200

    manage exceptions data file reading

--- learning.py
+++ learning.py
@@ -4,6 +4,22 @@ import matplotlib.pyplot as plt
 import statistics
 import sys
 
+def read_file (filename) :
+    try :
+        data = pd.read_csv(filename, sep=",")
+    except :
+        sys.exit("Error : data file", filename, "not found. Try another file")
+    columns = list(data.columns.values)
+    name_X = columns[0]
+    name_Y = columns[1]
+    if (not name_X.isalpha() or not name_Y.isalpha() ):
+        sys.exit("problem to read column labels : \nX column label = "+ name_X +" \nY column label = "+ name_Y + " \ntry another file")
+    X_raw = np.array(data[name_X].astype(float))
+    y = np.array(data[name_Y].astype(float))
+    if (len(X_raw) < 10 or len(y < 10)) :
+        sys.exit("not enough data to learn")
+    return X_raw, y, name_X, name_Y
+
 def centrer_reduire (X):
     stdev = statistics.stdev(X)
     mean = statistics.mean(X)
@@ -16,18 +32,20 @@ def centrer_reduire (X):
 def hipothesis(X, theta):
     return X*theta[1] + theta[0]
 
-def calcul_mean_error(errors, m):
-    mean_error = (1/(2*m))*(np.sum(errors**2))
-    return mean_error
+def calcul_cost(errors, m):
+    mean_error = np.sum((errors**2)**0.5)/m
+    total_cost = (1/(2*m))*(np.sum(errors**2))
+    return mean_error, total_cost
 
-def fit(X, y, theta, alpha, num_iters):
-    
+# learning function
+def learn(X, y, theta, alpha, num_iters):    
     #print("nombre d'enregistrements m = " , m)
     #print("pas d'apprentissage alpha = " , alpha)
     X0 = np.ones(len(X))
     m = len(X)   #m = X.shape[0]
     gradient = [0,0]
     E_history = []
+    C_history = []
     G_history = []
     T_history = []
     for _ in range(num_iters):
@@ -35,8 +53,8 @@ def fit(X, y, theta, alpha, num_iters):
         # calcul error between hipothesis and real data (Y)
         errors = hipothesis(X, theta) - y
         
-        # calcul total cost (sum of errors)
-        mean_error = calcul_mean_error(errors, m)
+        # calcul total cost (sum of errors) for gradient and mean error for prediction
+        mean_error, total_cost = calcul_cost(errors, m)
 
         #calcul gradient descent avec pas d'apprentissage alpha    
         gradient[0] = alpha * np.dot(errors, X0)/ m
@@ -48,11 +66,12 @@ def fit(X, y, theta, alpha, num_iters):
 
         # memorise this iteration
         E_history.append(mean_error)
+        C_history.append(total_cost)
         gradient_copy = gradient.copy()
         G_history.append(gradient_copy)
         theta_copy = theta.copy()
         T_history.append(theta_copy)
-    return theta, E_history, G_history, T_history 
+    return theta, E_history, C_history, G_history, T_history 
 
 
 def visualizeData(X, y, name_X, name_Y):  
@@ -89,99 +108,85 @@ def visualizeRegression(X, y, theta, name_X, name_Y):
 def visualizeHistory (history_list, history_name, figure_number) :
     plt.figure(figure_number)
     ax = plt.axes()
-    plt.title("Bonus : " + history_name + ' evolution', fontsize=18, fontweight='bold')
+    plt.title("Bonus : " + history_name + ' evolution', fontsize=16, fontweight='bold')
     plt.xlabel('Iteration', fontsize=14, fontweight='bold')
     plt.ylabel(history_name, fontsize=14, fontweight='bold')
     ax.plot(history_list)
     plt.show()
 
-def visualizeError (E_history) :
-    plt.figure(4)
-    ax = plt.axes()
-    plt.title('Bonus : Mean error evolution', fontsize=18, fontweight='bold')
-    plt.xlabel('Iteration', fontsize=14, fontweight='bold')
-    plt.ylabel('Error', fontsize=14, fontweight='bold')
-    ax.plot(E_history)
-    plt.show()
-
-def visualizeGradient (G_history) :
-    plt.figure(4)
-    ax = plt.axes()
-    plt.title('Bonus : Gradient evolution', fontsize=18, fontweight='bold')
-    plt.xlabel('Iteration', fontsize=14, fontweight='bold')
-    plt.ylabel('Gradient', fontsize=14, fontweight='bold')
-    ax.plot(G_history)
-    plt.show()
-
-def visualizeTheta (T_history) :
-    plt.figure(4)
-    ax = plt.axes()
-    plt.title('Bonus : Theta evolution', fontsize=18, fontweight='bold')
-    plt.xlabel('Iteration', fontsize=14, fontweight='bold')
-    plt.ylabel('Theta', fontsize=14, fontweight='bold')
-    ax.plot(T_history)
-    plt.show()
-
 def save_parameters (theta, mean, stdev, mean_error) :
     line = str(theta[0])+","+ str(theta[1]) + "," + str(mean)+","+ str(stdev)+","+ str(mean_error)
-    with open ("parameters.txt", "w" , encoding="utf-8") as file :
-        file.write(line)
-    file.close
+    try :
+        with open ("parameters.txt", "w" , encoding="utf-8") as file :
+            file.write(line)
+        file.close
+    except :
+        sys.exit("Error : File could not be saved")
 
-def main(argv):
+def main(filename):
+    
+    
+    
+    print("----------------------------------------------------------------------\n")
+    print("Phase I  READ DATA FOR LEARNING\n")
     # lecture des donnees
-    data = pd.read_csv(filename, sep=",")
-    columns = list(data.columns.values)
-    name_X = columns[0]
-    name_Y = columns[1]
-    X_raw = np.array(data[name_X].astype(float))
-    y = np.array(data[name_Y].astype(float))
+    X_raw , y , name_X, name_Y = read_file (filename)
+    print("\nFound data 'X' to learn : \ncolumn name =", name_X , X_raw)
+    print("\nFound data 'y' to predict : \ncolumn name =", name_Y, y)
     
-    print("Phase I  Lecture des donnees pour apprentissage")
-    print("Found data 'X' to learn : ", name_X , X_raw)
-    print("Found data 'y' to predict : ", name_Y, y)
-    
-    print("Bonus 1 : visualisation  des données bruts avant la normalisation")
+    # visualisation du dataset
+    print("\nBonus 1 : Visualize  dataset before normalization")
     #data.plot.scatter(name_X, name_Y)
     visualizeData(X_raw, y, name_X, name_Y)
     
-    print("Phase II Normalisation des donnees")
+    print("\n----------------------------------------------------------------------")
+
+    print("\n\nPhase II DATA NORMALIZATION")
     X_norm, mean, stdev = centrer_reduire(X_raw)
-    print("X normalized = ", X_norm)
+    print("\nX normalized = ", X_norm)
     
+    print("\n----------------------------------------------------------------------")
     
-    print("Phase III Apprentissage")
+    print("\n\nPhase III LARNING")
+    
+    #initialise 2 linear coefficients theta[1] et theta[0] a zero
     theta = np.zeros(2)
     alpha = 0.2
     iterations = 100
-    print("Coefficients  avant apprentissage" , theta)
-    print("Try to learn with alpha = ", alpha," nombre d'iterations =", iterations )
-    #theta = fit(X_norm, y, theta, 0.2, 100)
-    theta, E_history, G_history, T_history = fit(X_norm, y, theta, alpha, iterations)
-    print(E_history)
+    print("\nLinear coefficients before learning" , theta)
+    print("Try to learn with learning rate =", alpha," iterations number =", iterations )
+
+    theta, E_history, C_history, G_history, T_history = learn(X_norm, y, theta, alpha, iterations)
+    print("Learning acomplished")
+    print("Linear coefficients after learning" , theta)
     mean_error = E_history[-1]
-    print("Coefficients apres apprentissage" , theta)
-    print("Bonus 2 : Erreur d'apprentissage : ", mean_error)
+    print("\nBonus 2 : Mean error after learning: ", mean_error)
     
     # Learnig visualisations
-    print("Bonus 3 : visualise regression" )
+    print("Bonus 3 : Visualize linear regression" )
     visualizeRegression(X_norm, y, theta, name_X, name_Y)
-    print("Bonus 4 : visualise all iterations of coeficients search" )
-    visualizeHistory(T_history, "Coeficients Theta", 3)
-    print("Bonus 5 : visualise all iterations of gradient descend " )
+    print("Bonus 4 : Visualize all iterations of coeficients search" )
+    visualizeHistory(T_history, "Linear coefficients", 3)
+    print("Bonus 5 : Visualize all iterations of gradient descend " )
     visualizeHistory(G_history, "Gradient", 4)
-    print("Bonus 6 : visualise all iterations of mean error" )
-    visualizeHistory(E_history, "Mean Error", 5)
+    print("Bonus 6 : Visualize all iterations of mean error" )
+    visualizeHistory(E_history, "Mean error", 5)
+    visualizeHistory(C_history, "Total cost", 6)
     
+    print("\n----------------------------------------------------------------------")
+
     #saving results
-    print("Phase IV : Saving parameters ")
+    print("\n\nPhase IV : SAVING PARAMETERS ")
     save_parameters (theta, mean, stdev, mean_error)
-    print("parameters saved to file parameters.txt")
+    print("\nParameters saved to file parameters.txt")
+    print("Use predict.py [option:parameters.txt] to predict price")
+    
+    print("\n----------------------------------------------------------------------\n")
 
 if __name__ == '__main__':
-    filename = "price_data.csv"
+    filename = "data.csv"
     if len(sys.argv) == 2 :
         filename = sys.argv[1]
     elif len(sys.argv) > 2 :
-        sys.exit("Wrong number of parameters. expected one data file or nothing")
+        sys.exit("Error : Wrong number of parameters. expected one data file or nothing")
     main(filename)
